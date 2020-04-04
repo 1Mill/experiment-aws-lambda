1a08d2a25a9604a1e6ee0ceded8a5486a2a13312
1Mill <erik.ekberg@outlook.com>
2020-04-04 15:09:50 -0700

    Update index.js

--- src/my-test-function/index.js
+++ src/my-test-function/index.js
@@ -1,6 +1,6 @@
 console.log('Did a thing');
 
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, _callback) => {
 	console.log('EVENT: \n', JSON.stringify(event, null, 2));
 	return context.logStreamName
 }
