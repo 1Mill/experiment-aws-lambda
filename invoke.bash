0694bc6c63e173d6990dc251f11dc7d8a3eee184
1Mill <erik.ekberg@outlook.com>
2020-09-07 20:04:36 -0700

    Default to Event invocation type

--- invoke.bash
+++ invoke.bash
@@ -1,9 +1,11 @@
 echo "event.json: ${1}"
 echo "port: ${2}"
+echo "invocation-type ${3-Event}" # https://docs.aws.amazon.com/lambda/latest/dg/API_Invoke.html#API_Invoke_RequestSyntax
 
  aws lambda invoke \
 	--cli-binary-format raw-in-base64-out \
 	--function-name index \
+	--invocation-type ${3-Event} \
 	--no-sign-request \
 	--payload "$(cat ${1})" \
  	--endpoint http://localhost:${2} \
