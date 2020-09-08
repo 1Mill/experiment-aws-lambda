364c213881993950e74245f9f6b235a84a5d6489
1Mill <erik.ekberg@outlook.com>
2020-09-07 19:51:44 -0700

    Create script to more easily invoke functions

---
+++ invoke.bash
@@ -0,0 +1,10 @@
+ echo "event.json: ${1}"
+ echo "port: ${2}"
+
+ aws lambda invoke \
+	--cli-binary-format raw-in-base64-out \
+	--function-name index \
+	--no-sign-request \
+	--payload "$(cat ${1})" \
+ 	--endpoint http://localhost:${2} \
+	output.json
