18fa256570270137372f55ccbf3f77884168fc05
1Mill <erik.ekberg@outlook.com>
2020-09-08 21:42:03 -0700

    Create main.tf

---
+++ services/hello-world/main.tf
@@ -0,0 +1,54 @@
+terraform {
+	required_version = "~> 0.13.2"
+
+	backend "s3" {
+		// access_key = ENVIRONMENT AWS_ACCESS_KEY_ID
+		// region = ENVIRONMENT AWS_DEFAULT_REGION
+		// secret_key = ENVIRONMENT AWS_SECRET_ACCESS_KEY
+
+		bucket = "experiment-terraform-state"
+		dynamodb_table = "experiment-terraform-state-locks"
+		encrypt = true
+		key = "example-aws-lambda/hello-world/terraform.tfstate"
+	}
+
+	required_providers {
+		archive = {
+			source = "hashicorp/archive"
+			version = "~> 1.3.0"
+		}
+		aws = {
+			source = "hashicorp/aws"
+			version = "~> 3.5.0"
+		}
+	}
+}
+
+data "archive_file" "default" {
+	output_path = "${path.module}/lambda.zip"
+	source_file = "${path.module}/index.js"
+	type = "zip"
+}
+data "aws_iam_policy_document" "default" {
+	version = "2012-10-17"
+
+	statement {
+		actions = ["sts:AssumeRole"]
+		effect = "Allow"
+
+		principals {
+			identifiers = ["lambda.amazonaws.com"]
+			type = "Service"
+		}
+	}
+}
+resource "aws_iam_role" "default" {
+	assume_role_policy = data.aws_iam_policy_document.default.json
+}
+resource "aws_lambda_function" "default" {
+	function_name = "MyPlaceholderFunctionName"
+	handler = "index.handler"
+	role = aws_iam_role.default.arn
+	runtime = "nodejs12.x"
+	source_code_hash = data.archive_file.default.output_base64sha256
+}
