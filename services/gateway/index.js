87ed81c74c23c9f3b766438a6bc1b4fc51669060
1Mill <erik.ekberg@outlook.com>
2020-09-09 21:10:20 -0700

    Create rapids eventStream

--- services/gateway/index.js
+++ services/gateway/index.js
@@ -1,20 +1,28 @@
 const AWS = require('aws-sdk')
+const { v2: { createEventStream } } = require('@1mill/cloudevents')
+
+const rapids = createEventStream({
+	id: 'gateway',
+	protocol: process.env.CLOUDEVENTS_RAPIDS_PROTOCOL,
+	urls: (process.env.CLOUDEVENTS_RAPIDS_URLS || '').split(','),
+});
+console.log(rapids)
+
 AWS.config.update({
 	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 	region: process.env.AWS_DEFAULT_REGION,
 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 })
-
 const lambda = new AWS.Lambda({
 	apiVersion: '2015-03-31',
 	// endpoint: 'http://localhost:9001/',
 })
-
 const params = {
 	FunctionName: process.env.AWS_LAMBDA_ARN,
 	InvocationType: 'Event',
 	Payload: JSON.stringify({ todo: 'TODO' }), // TODO: Update to cloudevent payload
 }
+
 lambda.invoke(params, (err, data) => {
 	err ? console.error(err, err.stack) : console.log(data)
 })
