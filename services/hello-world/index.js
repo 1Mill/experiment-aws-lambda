f1836a971a89187286e5428d3e73b96a85408801
1Mill <erik.ekberg@outlook.com>
2020-09-10 21:12:47 -0700

    Install cloudevents on the lambda

--- services/hello-world/index.js
+++ services/hello-world/index.js
@@ -1,3 +1,9 @@
+const { v2: { isEnriched }} = require('@1mill/cloudevents')
+
 exports.handler = async ({ cloudevent }, _context, _callback) => {
+	// Escape clauses
+	if (isEnriched({ cloudevent })) { return }
+
+	// Buisness logic
 	console.log("EVENT: \n" + JSON.stringify(cloudevent, null, 2))
 }
