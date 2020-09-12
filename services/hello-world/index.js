2513c280b0c5ca15d02087aa9bc1fb8a3017cfba
1Mill <erik.ekberg@outlook.com>
2020-09-12 15:45:16 -0700

    Update index.js

--- services/hello-world/index.js
+++ services/hello-world/index.js
@@ -1,10 +1,10 @@
 const { v2: { isEnriched }} = require('@1mill/cloudevents')
 
 exports.handler = async ({ cloudevent }, _context, _callback) => {
-	// Escape clauses
+	// * Escape clauses
 	if (isEnriched({ cloudevent })) { return }
 
-	// Buisness logic
+	// * Buisness logic
 	console.log('EVENT: \n' + JSON.stringify(cloudevent, null, 2))
 
 	// ! Testing purposes only for InvocationType: 'RequestResponse'
