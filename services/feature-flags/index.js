4b064c88d13f4eda4c18008bff2af933a5fb72c9
1Mill <erik.ekberg@outlook.com>
2020-09-13 11:34:55 -0700

    Move things around + connect to rapids

--- services/feature-flags/index.js
+++ services/feature-flags/index.js
@@ -1,25 +1,37 @@
-const { v2: { isEnriched } } = require('@1mill/cloudevents')
+const { v2: {
+	createEventStream,
+	enrich,
+	isEnriched,
+} } = require('@1mill/cloudevents')
 
-const FLAGS = {
-	'another-my-flag': false,
-	'my-flag': true,
-}
+const rapids = createEventStream({
+	id: 'feature-flags-service',
+	mechanism: process.env.CLOUDEVENTS_RAPIDS_MECHANISM,
+	password: process.env.CLOUDEVENTS_RAPIDS_PASSWORD,
+	protocol: process.env.CLOUDEVENTS_RAPIDS_PROTOCOL,
+	urls: (process.env.CLOUDEVENTS_RAPIDS_URLS || '').split(','),
+	username: process.env.CLOUDEVENTS_RAPIDS_USERNAME,
+})
 
 exports.handler = async ({ cloudevent }, _context, _callback) => {
 	// * Escape clauses
 	if (isEnriched({ cloudevent })) { return }
 
-	// * Buisness logic
+	// * Business logic
 	const { name } = JSON.parse(cloudevent.data)
 	const enrichment = getFeatureFlagState({ flags: FLAGS, name })
 
-	// TODO: Emit enriched event to rapids
-	// rapids.emit({ cloudevent: enrich(cloudevent, enrichment )})
+	// * Publish enriched event to rapids
+	rapids.emit({ cloudevent: enrich(cloudevent, enrichment) })
 
 	// ! Testing purposes only for InvocationType: 'RequestResponse'
 	return enrichment
 }
 
+const FLAGS = {
+	'another-my-flag': false,
+	'my-flag': true,
+}
 const getFeatureFlagState = ({ flags, name }) => {
 	return { isEnabled: flags[name], name };
 }
