6225ae22fd9f94261b3e506494574a7c51319938
1Mill <erik.ekberg@outlook.com>
2020-09-24 20:27:45 -0700

    Scaffold emitter

--- services/emitter/index.js
+++ services/emitter/index.js
@@ -1,1 +1,26 @@
-console.log('testing')
+const {
+	v3: { createCloudevent, createEventStream },
+} = require('@1mill/cloudevents');
+
+const rapids = createEventStream({
+	id: 'services.emitter',
+	protocol: process.env.CLOUDEVENTS_RAPIDS_PROTOCOL,
+	urls: (process.env.CLOUDEVENTS_RAPIDS_URLS || '').split(','),
+})
+
+let id = 0
+setInterval(() => {
+	try {
+		id += 1;
+
+		console.log(`Emitting id: ${id}`);
+		const cloudevent = createCloudevent({
+			id,
+			source: 'emitter',
+			type: 'hello-world',
+		});
+		rapids.emit({ cloudevent })
+	} catch (err) {
+		console.error(err)
+	}
+}, 5000)
